1d380178c528712eaffaf7d08aa19a9f52b6f35f
Ivan Hladysh <hladysh.im@gmail.com>
2018-06-28 13:25:14 +0300

    add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
     notify = require("gulp-notify"),
+    sourcemaps = require('gulp-sourcemaps'),
     rsync = require('gulp-rsync');
 
 gulp.task('browser-sync', function () {
@@ -28,10 +29,12 @@ gulp.task('browser-sync', function () {
 
 gulp.task('styles', function () {
     return gulp.src('app/' + syntax + '/**/*.' + syntax + '')
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer(['last 15 versions']))
-        .pipe(cleancss({level: {1: {specialComments: 0}}})) // Opt., comment out when debugging
+        .pipe(sourcemaps.write('.'))
+        // .pipe(cleancss({level: {1: {specialComments: 0}}})) // Opt., comment out when debugging
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.stream())
 });
